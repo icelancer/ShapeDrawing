fd2fcef8c4a1045515d98adbcda0825d9991cdec
icelancer <SendtoGhkim@gmail.com>
2014-07-04 16:30:46 +0900

    matrix 적용.

--- src/com/icelancer/shapedrawing/MainActivity.java
+++ src/com/icelancer/shapedrawing/MainActivity.java
@@ -4,14 +4,13 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 
+import android.app.Activity;
+import android.os.Bundle;
+
 import com.icelancer.NotSupportedFileFormat;
 import com.icelancer.shapefile.ShapeReader;
 import com.icelancer.shapefile.shape.Point;
 import com.icelancer.shapefile.shape.Polyline;
-
-import android.app.Activity;
-import android.os.Bundle;
-import android.util.Log;
 
 public class MainActivity extends Activity {
 
--- src/com/icelancer/shapedrawing/ShapeView.java
+++ src/com/icelancer/shapedrawing/ShapeView.java
@@ -6,7 +6,9 @@ import java.util.List;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Matrix;
 import android.graphics.Paint;
+import android.util.Log;
 import android.view.View;
 
 import com.icelancer.shapefile.shape.Point;
@@ -14,23 +16,41 @@ import com.icelancer.shapefile.shape.Point;
 public class ShapeView extends View {
 	private Paint paint;
 	private List<float[]> pointList;
+	private Matrix matrix;
+	private double[] bbox;
 	
 	public ShapeView(Context context) {
 		super(context);
 		this.pointList = new ArrayList<float[]>();
 		this.paint = new Paint();
 		paint.setColor(Color.RED);
+		
+		this.matrix = new Matrix();
+		
+//		matrix.setRotate(180, this.getWidth()/2, this.getHeight()/2);
+		
 	}
 	
 	@Override
 	protected void onDraw(Canvas canvas) {
 		super.onDraw(canvas);
+		double mapWidth = bbox[2] - bbox[0];
+		double mapHeight = bbox[3] - bbox[1];
+		int screenWidth = canvas.getWidth();
+		int screenHeight = canvas.getHeight();
+		
+		matrix.setScale((float)(screenWidth/mapWidth), (float)(screenHeight/mapHeight));
+		
+		matrix.setRotate(180);
+		
+		canvas.setMatrix(matrix);
 		
 		for (float[] list: this.pointList) {
 			canvas.drawLines(list, paint);
 		}
 		
-
+		
+		
 	}
 	
 	public void drawPolyline (List<List<Point>> partsList, double[] bbox) {
@@ -42,14 +62,14 @@ public class ShapeView extends View {
 			idx = 0;
 			
 			for (Point point: list) {
-				pointList[idx++] = ((float) point.getX() - (float)bbox[0]) / 800;
-				pointList[idx++] = ((float) point.getY() - (float)bbox[1]) / 800;
-				
-//				Log.i("SHAPE", point.getX() + " = " + ((float) point.getX() - (float)bbox[0]));
+				pointList[idx++] = ((float) point.getX() - (float)bbox[0]);
+				pointList[idx++] = ((float) point.getY() - (float)bbox[1]);
 			}
 			
 			this.pointList.add(pointList);
 		}
+		
+		this.bbox = bbox;
 		
 	}
 	

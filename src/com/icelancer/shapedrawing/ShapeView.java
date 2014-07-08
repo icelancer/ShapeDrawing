e01dcce4e4319fe43e8e1084efd6a4056ea48b3a
김광호 <icelancer@naverui-iMac-3.local>
2014-07-08 19:04:01 +0900

    핀치 줌에 따른 스케일 처리.

--- src/com/icelancer/shapedrawing/ShapeView.java
+++ src/com/icelancer/shapedrawing/ShapeView.java
@@ -9,8 +9,12 @@ import android.graphics.Color;
 import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.RectF;
+import android.util.FloatMath;
 import android.util.Log;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
 import android.view.View;
+import android.view.ScaleGestureDetector.OnScaleGestureListener;
 
 import com.icelancer.shapefile.shape.Point;
 
@@ -20,6 +24,8 @@ public class ShapeView extends View {
 	private Matrix matrix;
 	private RectF drawableRect;
 	private RectF viewRect;
+	private ScaleGestureDetector detector;
+	
 	
 	public ShapeView(Context context) {
 		super(context);
@@ -30,21 +36,54 @@ public class ShapeView extends View {
 		this.matrix = new Matrix();
 		
 		Log.i(VIEW_LOG_TAG, this.getWidth()+"");
+		
+		this.detector = new ScaleGestureDetector(this.getContext(), new ScaleGestureDetector.OnScaleGestureListener(){
+
+			@Override
+			public boolean onScale(ScaleGestureDetector detector) {
+				
+				return false;
+			}
+
+			@Override
+			public boolean onScaleBegin(ScaleGestureDetector detector) {
+				// TODO Auto-generated method stub
+				return false;
+			}
+
+			@Override
+			public void onScaleEnd(ScaleGestureDetector detector) {
+				// TODO Auto-generated method stub
+				
+			}});
+		
+		this.setOnTouchListener(new OnTouchListener() {
+			private float initDistance;
+			
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+				detector.onTouchEvent(event);
+				return true;
+			}
+
+		});
 	}
+	
 	
 	@Override
 	protected void onSizeChanged(int w, int h, int oldw, int oldh) {
 		super.onSizeChanged(w, h, oldw, oldh);
-		this.viewRect = new RectF(0, 0, w, h);
+		
+		this.viewRect = new RectF(0, 0, this.getWidth(), this.getHeight());
+		matrix.setRectToRect(drawableRect, viewRect, Matrix.ScaleToFit.CENTER);
+		matrix.postRotate(180, this.getWidth()/2, this.getHeight()/2);
+		invalidate();
 	}
 	
 	@Override
 	protected void onDraw(Canvas canvas) {
 		super.onDraw(canvas);
 		
-		matrix.setRectToRect(drawableRect, viewRect, Matrix.ScaleToFit.CENTER);
-		
-		matrix.postRotate(180, this.getWidth()/2, this.getHeight()/2);
 		canvas.concat(matrix);
 		
 		for (float[] list: this.pointList) {
@@ -74,6 +113,11 @@ public class ShapeView extends View {
 		
 		
 		this.drawableRect = new RectF(0, 0, mapWidth, mapHeight);
+		
+		
+		
+		invalidate();
 	}
-	
+
 }
+
